a396ba26286aca570f4e577c2f09e009064c7b1b
afeinber <alexandernfeinberg@gmail.com>
2014-07-23 13:45:03 -0400

    Now counts of items are displayed

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
   TodoApp.TodoList.init();
+  $(document).ajaxStop(function() {
+    $('#saved-count').empty().text(TodoApp.TodoList.todones.length);
+    $('#unsaved-count').empty().text(TodoApp.TodoList.todos.length);
+  });
 });
